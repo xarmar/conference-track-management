b1f633342f77f0638078fe49a724ddc453f8ffa1
Xavier Marques <xavierpmarques@gmail.com>
2021-09-14 20:36:49 +0100

    Validate talkAssignedToTrack Method works within arrays.

--- src/components/Talk.unit.test.ts
+++ src/components/Talk.unit.test.ts
@@ -18,10 +18,35 @@ describe("talkAssignedToTrack Method returns true and false when expected", () =
         let reactForever = new Talk(20, 'React Forever', null);
         expect(reactForever.talkAssignedToTrack()).toBe(false);
     });
+
     test('talkAssignedToTrack returns true when track.hasPlace = true', () => {
         let awesomeTypeScript = new Talk(20, 'Awesome Typescript', null);
         awesomeTypeScript.hasSpot = true;
         expect(awesomeTypeScript.talkAssignedToTrack()).toBe(true);
     });
+    
+    test('talkAssignedToTrack works with arrays', () => {
+        let loveCoding = new Talk(20, 'Love Coding', null);
+        loveCoding.hasSpot = true;
+        let thisProject = new Talk(20, 'This Project', null);
+        thisProject.hasSpot = true;
+        let dogsAreAwesome = new Talk(20, 'Why dogs are awesome', null);
+        dogsAreAwesome.hasSpot = true;
+        let thisTalkWillReturnFalse = new Talk(20, 'I will return false', null);
+
+        // Create Dummy Arrays
+        let awesomeArrayThatWillPass = [loveCoding, thisProject, dogsAreAwesome];
+        let thisArrayWillFailTest = [loveCoding, thisProject, dogsAreAwesome, thisTalkWillReturnFalse];
+
+        
+        expect(awesomeArrayThatWillPass.every(talk => {
+            return talk.talkAssignedToTrack();
+        })).toBe(true);
+
+        expect(thisArrayWillFailTest.every(talk => {
+            return talk.talkAssignedToTrack();
+        })).toBe(false);
+    });
+
 })
 
