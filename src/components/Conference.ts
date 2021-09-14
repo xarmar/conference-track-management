d1f250703da74e25894dfdf3827ca30a78be2afc
Xavier Marques <xavierpmarques@gmail.com>
2021-09-14 19:17:27 +0100

    Add missing attributes to Conference Component

--- src/components/Conference.ts
+++ src/components/Conference.ts
@@ -3,11 +3,21 @@ import Track from './Track';
 
 class Conference extends Component<any,any> {
 
+    dayCounter: number;
+    morningStartTime: Date;
+    afternoonStartTime: Date;
     tracks: Track[]
     
     constructor(tracks: Track[], props: any) {
         super(props);
+        this.dayCounter = 0;
         this.tracks = tracks;
+
+        // Set Schedule For Conference
+        this.morningStartTime = new Date();
+        this.afternoonStartTime = new Date();
+        this.morningStartTime.setHours(9,0,0);
+        this.afternoonStartTime.setHours(13,0,0);
     }
 
     render() {
