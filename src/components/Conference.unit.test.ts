fcfea5c9a110c160c4fde9239a54e37e4299dff3
Xavier Marques <xavierpmarques@gmail.com>
2021-09-14 19:39:05 +0100

    Add assertive languages for test description.

--- src/components/Conference.unit.test.ts
+++ src/components/Conference.unit.test.ts
@@ -1,8 +1,10 @@
 import Conference from "./Conference";
 
 describe("It is possible to create a 'Conference' object with the 'new' keyword", () => {
-    test('creates a new Conference object', () => {
+    test('creates a new Conference object with the expected attributes', () => {
         let onlyOneConference = new Conference([], null);
+        
+        // Expected Values of Dates
         let morningStartingTime = new Date();
         morningStartingTime.setHours(9,0,0);
         let afternoonStartTime = new Date();
--- src/components/Track.unit.test.ts
+++ src/components/Track.unit.test.ts
@@ -1,7 +1,7 @@
 import Track from "./Track";
 
 describe("It is possible to create a 'Track' object with the 'new' keyword", () => {
-    test('creates a new Track object', () => {
+    test('creates a new Track object with the expected attributes', () => {
 
         let morningStartTime = new Date();
         morningStartTime.setHours(9,0,0);
