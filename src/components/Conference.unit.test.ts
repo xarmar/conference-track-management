7edbf550b0a7a93ea09925aa7858a753046f31c9
Xavier Marques <xavierpmarques@gmail.com>
2021-09-14 19:17:57 +0100

    Add missing attributes to unit test.

--- src/components/Conference.unit.test.ts
+++ src/components/Conference.unit.test.ts
@@ -3,8 +3,16 @@ import Conference from "./Conference";
 describe("It is possible to create a 'Conference' object with the 'new' keyword", () => {
     test('creates a new Conference object', () => {
         let onlyOneConference = new Conference([], null);
+        let morningStartingTime = new Date();
+        morningStartingTime.setHours(9,0,0);
+        let afternoonStartTime = new Date();
+        afternoonStartTime.setHours(13,0,0);
+
         expect(onlyOneConference).toEqual(expect.objectContaining({
-            tracks: []
+            dayCounter: 0,
+            morningStartTime: morningStartingTime,
+            afternoonStartTime: afternoonStartTime,
+            tracks:  []
         }));
     });
 })
