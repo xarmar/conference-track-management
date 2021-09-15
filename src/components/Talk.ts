3163ee56ae32cd7a76dbcd9993f59a21777a0196
Xavier Marques <xavierpmarques@gmail.com>
2021-09-15 21:28:52 +0100

    Write  placeTalk method and corresponding unit test.

--- src/components/Talk.ts
+++ src/components/Talk.ts
@@ -1,4 +1,7 @@
 import React, {Component} from 'react'
+import { addMinutesToDate } from '../dateManipulation/timeOperations';
+import Track from './Track';
+
 
 class Talk extends Component<any, any> {
 
@@ -15,10 +18,6 @@ class Talk extends Component<any, any> {
         this.startTime = undefined;
     }
 
-    render() {
-        return null
-    }
-
     // Checks if a Talk is already assigned to a Track
     talkAssignedToTrack() {
         if(this.hasSpot) {
@@ -26,6 +25,51 @@ class Talk extends Component<any, any> {
         }
         return false
     }
+
+    // Tries to place a Talk inside a target Track
+    placeTalk(track: Track) {
+        // Check if talk fits inside track's morning
+        if (this.duration <= track.sessions.morning.availableMinutes) {
+            
+            // Set startTime of Talk
+            this.startTime = track.sessions.morning.startTime;
+
+            // Increment startTime in Track to be ready for the next Track placement
+            track.sessions.morning.startTime = addMinutesToDate(track.sessions.morning.startTime, this.duration);
+
+            // Subtrack from track.sessions.morning.availableMinutes
+            track.sessions.morning.availableMinutes -= this.duration;
+
+            // Add talk to track's morning array
+            track.sessions.morning.talks.push(this);
+
+            // Tell App the talk is placed
+            this.hasSpot = true
+        }
+
+        else if (this.duration <= track.sessions.afternoon.availableMinutes) {
+
+            // Set startTime of Talk
+            this.startTime = track.sessions.afternoon.startTime;
+
+            // Increment startTime in Track to be ready for the next Track placement
+            track.sessions.afternoon.startTime = addMinutesToDate(track.sessions.afternoon.startTime, this.duration);
+
+            // Subtrack from track.sessions.afternoon.availableMinutes
+            track.sessions.afternoon.availableMinutes -= this.duration;
+
+            // Add talk to track's afternoon array
+            track.sessions.afternoon.talks.push(this);
+
+            // Tell App the talk is placed
+            this.hasSpot = true
+        }
+    }
+
+    render() {
+        return null
+    }
+
 }
 
 export default Talk
--- src/components/Talk.unit.test.ts
+++ src/components/Talk.unit.test.ts
@@ -1,4 +1,6 @@
+import { createDate } from "../dateManipulation/timeOperations";
 import Talk from "./Talk";
+import Track from "./Track";
 
 describe("It is possible to create a 'Talk' object with the 'new' keyword", () => {
     test('creates a new Talk object with the expected attributes', () => {
@@ -45,3 +47,62 @@ describe("talkAssignedToTrack Method returns true and false when expected", () =
         })).toBe(false);
     });
 })
+
+
+describe("placeTalk Method places Talks correctly", () => {
+    test('placeTalk correctly places a Talk when possible', () => {
+
+        let morningDate = createDate(9,0,0);
+        let afternoonDate = createDate(13,0,0);    
+
+        // Create Track
+        let trackOneMorningAvailable = new Track(1, morningDate, afternoonDate, null);
+        
+        // Pretend Track has time constraits
+        trackOneMorningAvailable.sessions.morning.availableMinutes = 66;
+        trackOneMorningAvailable.sessions.afternoon.availableMinutes = 0;
+
+        // Create Talks that will try to fit in track 1
+        let reactForever = new Talk(60, 'React Forever', null);
+        let iWontFitinTrackOne = new Talk(5, 'Fast Talk', null);
+
+        // Try to place tracks
+        reactForever.placeTalk(trackOneMorningAvailable);
+        iWontFitinTrackOne.placeTalk(trackOneMorningAvailable);
+
+        expect(reactForever.hasSpot).toBe(true);
+        expect(iWontFitinTrackOne.hasSpot).toBe(true);
+    });
+
+    test('placeTalk correctly places a Talk when possible and reject another when appropriate', () => {
+
+        let morningDate = createDate(9,0,0);
+        let afternoonDate = createDate(13,0,0);   
+        
+        // Create Track
+        let trackTwoAfternoonAvailable = new Track(2, morningDate, afternoonDate, null);
+
+        // Pretend Track has time constraits
+        trackTwoAfternoonAvailable.sessions.morning.availableMinutes = 0;
+        trackTwoAfternoonAvailable.sessions.afternoon.availableMinutes = 120;
+
+        // Create Talks that will try to fit in track 2
+        let iLoveJest = new Talk(60, 'I love Jest', null);
+        let iGoTrackTwo = new Talk(60, 'I go track two', null);
+        let iWontFitInTrackTwo = new Talk(5, 'I wont fit', null);
+
+        // Try to place tracks
+        iLoveJest.placeTalk(trackTwoAfternoonAvailable);
+        iGoTrackTwo.placeTalk(trackTwoAfternoonAvailable);
+        iWontFitInTrackTwo.placeTalk(trackTwoAfternoonAvailable);
+
+        expect(iLoveJest.hasSpot).toBe(true);
+        expect(iGoTrackTwo.hasSpot).toBe(true);
+        expect(iWontFitInTrackTwo.hasSpot).toBe(false);
+
+    });
+
+})
+
+
+
