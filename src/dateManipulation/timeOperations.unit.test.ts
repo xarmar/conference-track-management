c77f9841a5a730c98aa4fa1c98b61c36ea1736e3
Xavier Marques <xavierpmarques@gmail.com>
2021-09-15 20:17:14 +0100

    Fix naming of function to avoid conflicting with date-fns

--- src/dateManipulation/timeOperations.unit.test.ts
+++ src/dateManipulation/timeOperations.unit.test.ts
@@ -17,17 +17,17 @@ describe("createDate() creates a Date as expected", () => {
 describe("addMinutes() correctly add minutes to a Date", () => {
     test("Adding 50 minutes to 9AM === 9:50 AM", () => {
         const nineAm = timeOperation.createDate(9,0,0);
-        const nineFiftyAm = addMinutes(nineAm, 50);
+        const nineFiftyAm = timeOperation.addMinutesToDate(nineAm, 50);
         expect(timeOperation.convertToAmPm(nineFiftyAm)).toBe("9:50 AM");
     });
     test("Adding 23 minutes to 2PM === 2:23 AM", () => {
         const twoPm = timeOperation.createDate(14,0,0);
-        const twoTwentyThree = addMinutes(twoPm, 23);
+        const twoTwentyThree = timeOperation.addMinutesToDate(twoPm, 23);
         expect(timeOperation.convertToAmPm(twoTwentyThree)).toBe("2:23 PM");
     });
     test("Adding 11 minutes to 11PM === 11:11 PM", () => {
         const elevenPm = timeOperation.createDate(23,0,0);
-        const elevenEleven = addMinutes(elevenPm, 11);
+        const elevenEleven = timeOperation.addMinutesToDate(elevenPm, 11);
         expect(timeOperation.convertToAmPm(elevenEleven)).toBe("11:11 PM");
     });
 });
