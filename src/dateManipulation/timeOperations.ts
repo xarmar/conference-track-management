b408865438fdeb39a0e9106c96fa499c7509ac67
Xavier Marques <xavierpmarques@gmail.com>
2021-09-15 23:04:14 +0100

    Add subtrackMinutesFromDate function and corresponding unit test
    * Clean up timeOperations.unit.test.ts.
    * Use beforeEach() to keep code DRY

--- src/dateManipulation/timeOperations.ts
+++ src/dateManipulation/timeOperations.ts
@@ -1,6 +1,7 @@
 import addMinutes from 'date-fns/addMinutes'
 import format from 'date-fns/format';
 import set from 'date-fns/set'
+import subMinutes from 'date-fns/subMinutes'
 
 export const createDate = (hours: number, minutes: number, seconds: number) => {
     const newDate = set(new Date(), {hours: hours, minutes: minutes, seconds: seconds});  
@@ -14,5 +15,10 @@ export const convertToAmPm = (date: Date) => {
 
 export const addMinutesToDate = (originalDate: Date, minutesToAdd: number) => {
     const manipulatedDate = addMinutes(originalDate, minutesToAdd);
+    return manipulatedDate
+};
+
+export const subtrackMinutesFromDate = (originalDate: Date, minutesToSubtrack: number) => {
+    const manipulatedDate = subMinutes(originalDate, minutesToSubtrack);
     return manipulatedDate
 };
--- src/dateManipulation/timeOperations.unit.test.ts
+++ src/dateManipulation/timeOperations.unit.test.ts
@@ -1,13 +1,20 @@
 import { addMinutes } from "date-fns";
-import { addMinutesToDate, convertToAmPm, createDate } from "./timeOperations";
+import { addMinutesToDate, convertToAmPm, createDate, subtrackMinutesFromDate } from "./timeOperations";
+
+var nineAm: Date;
+var twoPm: Date;
+var elevenPm: Date;
+
+// Before Each to Keep Code DRY
+beforeEach(() => {
+    nineAm = createDate(9,0,0);
+    twoPm = createDate(14,0,0);
+    elevenPm = createDate(23,0,0);
+});
 
 
 describe("createDate() creates a Date as expected", () => {
     test("Date has the correct time", () => {
-        const nineAm = createDate(9,0,0);
-        const twoPm = createDate(14,0,0);
-        const elevenPm = createDate(23,0,0);
-
         expect(convertToAmPm(nineAm)).toBe("9:00 AM");
         expect(convertToAmPm(twoPm)).toBe("2:00 PM");
         expect(convertToAmPm(elevenPm)).toBe("11:00 PM");
@@ -16,21 +23,32 @@ describe("createDate() creates a Date as expected", () => {
 
 describe("addMinutes() correctly add minutes to a Date", () => {
     test("Adding 50 minutes to 9AM === 9:50 AM", () => {
-        const nineAm = createDate(9,0,0);
         const nineFiftyAm = addMinutesToDate(nineAm, 50);
         expect(convertToAmPm(nineFiftyAm)).toBe("9:50 AM");
     });
-    test("Adding 23 minutes to 2PM === 2:23 AM", () => {
-        const twoPm = createDate(14,0,0);
+    test("Adding 23 minutes to 2PM === 2:23 PM", () => {
         const twoTwentyThree = addMinutesToDate(twoPm, 23);
         expect(convertToAmPm(twoTwentyThree)).toBe("2:23 PM");
     });
     test("Adding 11 minutes to 11PM === 11:11 PM", () => {
-        const elevenPm = createDate(23,0,0);
         const elevenEleven = addMinutesToDate(elevenPm, 11);
         expect(convertToAmPm(elevenEleven)).toBe("11:11 PM");
     });
 });
 
+describe("subtrackMinutes() subtracks correctly from a Date", () => {
+    test("Subtracting 50 minutes from 9AM === 8:10 AM", () => {
+        const eightTenAm = subtrackMinutesFromDate(nineAm, 50);
+        expect(convertToAmPm(eightTenAm)).toBe("8:10 AM");
+    });
+    test("Subtracting 23 minutes from 2PM === 1:37 PM", () => {
+        const oneThirtySeven = subtrackMinutesFromDate(twoPm, 23);
+        expect(convertToAmPm(oneThirtySeven)).toBe("1:37 PM");
+    });
+    test("Subtracting 11 minutes from 11PM === 10:49 PM", () => {
+        const elevenEleven = subtrackMinutesFromDate(elevenPm, 11);
+        expect(convertToAmPm(elevenEleven)).toBe("10:49 PM");
+    });
+});
 
 
