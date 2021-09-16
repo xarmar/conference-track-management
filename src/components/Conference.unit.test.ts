4facb766f25afbba0a7448455030b88973131d11
Xavier Marques <xavierpmarques@gmail.com>
2021-09-16 20:45:22 +0100

    Fix failing Jest test. Add beforeEach() to keep code DRY

--- src/components/Conference.unit.test.ts
+++ src/components/Conference.unit.test.ts
@@ -2,30 +2,38 @@
 import { createDate } from "../dateManipulation/timeOperations";
 import Conference from "./Conference";
 
+var onlyOneConference: Conference
+var expectedMorningStartTime: Date;
+var expectedMorningEndTalksBy: Date;
+var expectedAfternoonStartTime: Date;
+var expectedAfternoonEndTalksBy: Date;
+
+// Before Each to Keep Code DRY
+beforeEach(() => {
+    onlyOneConference = new Conference([], null);
+    expectedMorningStartTime = createDate(9,0,0);
+    expectedMorningEndTalksBy = createDate(12,0,0);
+    expectedAfternoonStartTime = createDate(13,0,0);
+    expectedAfternoonEndTalksBy = createDate(17,0,0);
+});
+
+// Test Class Object Creation
 describe("It is possible to create a 'Conference' object with the 'new' keyword", () => {
     test('creates a new Conference object with the expected attributes', () => {
-        let onlyOneConference = new Conference([], null);
-        
-        // Expected Values of Dates
-        let morningStartingTime = createDate(9,0,0);
-        let afternoonStartingTime = createDate(13,0,0);
-        let morningFinishTime = createDate(12,0,0);
-        let afternoonFinishTime = createDate(17,0,0);
-
         expect(onlyOneConference).toEqual(expect.objectContaining({
             trackCounter: 0,
-            morningStartTime: morningStartingTime,
-            afternoonStartTime: afternoonStartingTime,
-            finishMorningTalksBy: morningFinishTime,
-            finishAfternoonTalksBy: afternoonFinishTime,
+            morningStartTime: expectedMorningStartTime,
+            afternoonStartTime: expectedAfternoonStartTime,
+            finishMorningTalksBy: expectedMorningEndTalksBy,
+            finishAfternoonTalksBy: expectedAfternoonEndTalksBy,
             tracks: []
         }));
     });
-})
+});
 
+// Test createNewTrack Method
 describe("Method createNewTrack() works as Expected", () => {
     test('createNewTrack() creates a new Track object and adds it to the array', () => {
-        let onlyOneConference = new Conference([], null);
         expect(onlyOneConference.createNewTrack()).toEqual(expect(onlyOneConference.tracks.length).toBe(1));
     });
 });
