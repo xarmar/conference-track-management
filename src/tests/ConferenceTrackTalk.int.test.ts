07a570c010480940b106e3590b5291a823ee2192
Xavier Marques <xavierpmarques@gmail.com>
2021-09-16 09:12:25 +0100

    Fix failing Jest test.
    * Have test contain the missing elements.

--- src/tests/ConferenceTrackTalk.int.test.ts
+++ src/tests/ConferenceTrackTalk.int.test.ts
@@ -14,8 +14,6 @@ var trackExampleOne : Track;
 var trackExampleTwo : Track;
 var trackExampleThree : Track;
 
-
-
 // Before Each to Keep Code DRY
 beforeEach(() => {
     morningStartTime = createDate(9,0,0);
@@ -30,14 +28,14 @@ beforeEach(() => {
 });
 
 describe("Conference Component can keep track of it's children Tracks", () => {
+
+    test('Check if Tracks were corretly added to Conference tracks arrays', () => {
     
     // Create dummy Conference
     let onlyOneConference = new Conference([], null);
 
     // Add Tracks to Conference array
     onlyOneConference.tracks.push(trackExampleOne, trackExampleTwo, trackExampleThree)
-
-    test('Check if Tracks were corretly added to Conference tracks arrays', () => {
         expect(onlyOneConference).toEqual(expect.objectContaining({
             tracks: [trackExampleOne, trackExampleTwo, trackExampleThree]
         }));
@@ -46,17 +44,17 @@ describe("Conference Component can keep track of it's children Tracks", () => {
 });
 
 describe("Track Component can keep track of it's children Talks", () => {
-    
-    // Create dummy Talks
-    let loveCoding = new Talk(60, 'Why I love coding', null);
-    let livingInGermany = new Talk(45, 'Living in Germany', null);
-    let afternoonCoffee = new Talk (30, 'Afternoon Coffee', null);
-
-    // Add Talks to Track arrays
-    trackExampleOne.sessions.morning.talks.push(loveCoding, livingInGermany);
-    trackExampleOne.sessions.afternoon.talks.push(afternoonCoffee);
 
     test('Check if Talks were corretly added to Track arrays', () => {
+        // Create dummy Talks
+        let loveCoding = new Talk(60, 'Why I love coding', null);
+        let livingInGermany = new Talk(45, 'Living in Germany', null);
+        let afternoonCoffee = new Talk (30, 'Afternoon Coffee', null);
+
+        // Add Talks to Track arrays
+        trackExampleOne.sessions.morning.talks.push(loveCoding, livingInGermany);
+        trackExampleOne.sessions.afternoon.talks.push(afternoonCoffee);
+
         expect(trackExampleOne.sessions.morning).toEqual(expect.objectContaining({
             talks: [loveCoding, livingInGermany]
         }));
