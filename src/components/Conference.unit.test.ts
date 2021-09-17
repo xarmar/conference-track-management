8f2ab1b80a38dfc311cb0f052a0cffda020cb362
Xavier Marques <xavierpmarques@gmail.com>
2021-09-17 09:10:35 +0100

    Move buildTrackList() test to integration,test file

--- src/components/Conference.unit.test.ts
+++ src/components/Conference.unit.test.ts
@@ -41,14 +41,14 @@ describe("It is possible to create a 'Conference' object with the 'new' keyword"
     });
 });
 
-// Method createNewTrack()
+// Method createNewTrack() from Conference.ts
 describe("Method createNewTrack() works as Expected", () => {
     test('createNewTrack() creates a new Track object and adds it to the array', () => {
         expect(onlyOneConference.createNewTrack()).toEqual(expect(onlyOneConference.tracks.length).toBe(1));
     });
 });
 
-// Method placeNetworkingEvents()
+// Method placeNetworkingEvents() from Conference.ts
 describe("Method placeNetworkingEvents() works as Expected", () => {
     test('placeNetworkingEvents() correctly determines the startTime of Networking events', () => {
         // Pretend TrackS have time constraits
@@ -66,40 +66,5 @@ describe("Method placeNetworkingEvents() works as Expected", () => {
         expect(trackExampleOne.networkingEventStartTime).toBe('4:00 PM');
         expect(trackExampleTwo.networkingEventStartTime).toBe('4:20 PM');
         expect(trackExampleThree.networkingEventStartTime).toBe('5:00 PM');
-    });
-});
-
-// Method buildTrackList()
-describe("Method buildTrackList() works as Expected", () => {
-    test('buildTrackList() gives out the same output as the one provided in the PDF document', () => {
-
-        let writingFastTestsAgainstEnterpriseRails = new Talk(60, 'Writing Fast Tests Against Enterprise Rails', null);
-        let overdoingitinPython = new Talk(45, 'Overdoing it in Python', null);
-        let luafortheMasses = new Talk(30, 'Lua for the Masses', null);
-        let rubyErrorsfromMismatchedGemVersions = new Talk(45, 'Ruby Errors from Mismatched Gem Versions', null);
-        let commonRubyErrors = new Talk (45, 'Common Ruby Errors', null);
-        let railsforPythonDevelopers = new Talk(5, 'Rails for Python Developers', null);
-        let communicatingOverDistance = new Talk (60, 'Communicating Over Distance', null);
-        let accountingDrivenDevelopment = new Talk(45, 'Accounting-Driven Development', null);
-        let woah = new Talk(30, 'Woah', null);
-        let sitDownandWrite = new Talk(30, 'Sit Down and Write', null);
-        let pairProgrammingvsNoise = new Talk (45, 'Pair Programming vs Noise', null);
-        let railsMagic = new Talk (60, 'Rails Magic', null);
-        let rubyonRailsWhyWeShouldMoveOn = new Talk (60, 'Ruby on Rails: Why We Should Move On', null);
-        let clojureAteScalaonmyproject = new Talk(45, 'Clojure Ate Scala (on my project)', null);
-        let programmingintheBoondocksofSeattle = new Talk(30, 'Programming in the Boondocks of Seattle', null);
-        let rubyVsClojureforBackEndDevelopment = new Talk(30, 'Ruby vs. Clojure for Back-End Development', null );
-        let rubyonRailsLegacyAppMaintenance = new Talk (60,'Ruby on Rails Legacy App Maintenance', null);
-        let aWorldWithoutHackerNews = new Talk (30, 'A World Without HackerNews', null);
-        let userInterfaceCSSinRailsApps = new Talk(30, 'User Interface CSS in Rails Apps', null);
-
-        // Add Talks to arrayOfTalks
-        let arrayOfTalks = [writingFastTestsAgainstEnterpriseRails, overdoingitinPython, luafortheMasses, rubyErrorsfromMismatchedGemVersions, 
-            commonRubyErrors, railsforPythonDevelopers, communicatingOverDistance, accountingDrivenDevelopment, woah, sitDownandWrite, 
-            pairProgrammingvsNoise, railsMagic, rubyonRailsWhyWeShouldMoveOn, clojureAteScalaonmyproject, programmingintheBoondocksofSeattle, 
-            rubyVsClojureforBackEndDevelopment, rubyonRailsLegacyAppMaintenance, aWorldWithoutHackerNews, userInterfaceCSSinRailsApps];
-
-        onlyOneConference.buildTrackList(arrayOfTalks);
-        expect(onlyOneConference.tracks).toHaveLength(2);
     });
 });
--- src/tests/ConferenceTrackTalk.int.test.ts
+++ src/tests/ConferenceTrackTalk.int.test.ts
@@ -5,26 +5,72 @@ import { createDate } from "../dateManipulation/timeOperations";
 
 // This file does integration testing between the three Componentes: Conference, Track and Talk
 
+var onlyOneConference: Conference;
 var morningStartTime: Date;
 var morningEndTalksBy: Date;
 var afternoonStartTime: Date;
 var afternoonEndTalksBy: Date;
-var timeInAmPm : String;
 var trackExampleOne : Track;
 var trackExampleTwo : Track;
 var trackExampleThree : Track;
 
+// Initiate variables that will hold Talks
+var writingFastTestsAgainstEnterpriseRails: Talk
+var overdoingitinPython: Talk
+var luafortheMasses: Talk
+var rubyErrorsfromMismatchedGemVersions: Talk
+var commonRubyErrors: Talk
+var railsforPythonDevelopers: Talk
+var communicatingOverDistance: Talk
+var accountingDrivenDevelopment: Talk
+var woah: Talk
+var sitDownandWrite: Talk
+var pairProgrammingvsNoise: Talk
+var railsMagic: Talk
+var rubyonRailsWhyWeShouldMoveOn: Talk
+var clojureAteScalaonmyproject: Talk
+var programmingintheBoondocksofSeattle: Talk
+var rubyVsClojureforBackEndDevelopment: Talk
+var rubyonRailsLegacyAppMaintenance: Talk
+var aWorldWithoutHackerNews: Talk
+var userInterfaceCSSinRailsApps: Talk
+
 // Before Each to Keep Code DRY
 beforeEach(() => {
+    // Create 'start' and 'endBy' times for each session
     morningStartTime = createDate(9,0,0);
     morningEndTalksBy = createDate(12,0,0);
     afternoonStartTime = createDate(13,0,0);
     afternoonEndTalksBy = createDate(17,0,0);
-    timeInAmPm = "";
+
+    // Create Conference to use in testing
+    onlyOneConference = new Conference([], null);
+
+    // Create Tracks to use in testing
     trackExampleOne = new Track(1, morningStartTime, afternoonStartTime, morningEndTalksBy, afternoonEndTalksBy, null);
     trackExampleTwo = new Track(2, morningStartTime, afternoonStartTime, morningEndTalksBy, afternoonEndTalksBy, null);
     trackExampleThree = new Track(3, morningStartTime, afternoonStartTime, morningEndTalksBy, afternoonEndTalksBy, null);
 
+    // Create Talks to use in testing
+    writingFastTestsAgainstEnterpriseRails = new Talk(60, 'Writing Fast Tests Against Enterprise Rails', null);
+    overdoingitinPython = new Talk(45, 'Overdoing it in Python', null);
+    luafortheMasses = new Talk(30, 'Lua for the Masses', null);
+    rubyErrorsfromMismatchedGemVersions = new Talk(45, 'Ruby Errors from Mismatched Gem Versions', null);
+    commonRubyErrors = new Talk (45, 'Common Ruby Errors', null);
+    railsforPythonDevelopers = new Talk(5, 'Rails for Python Developers', null); // don't forget to add this.isLightning: Boolean to the constructor of Talks
+    communicatingOverDistance = new Talk (60, 'Communicating Over Distance', null);
+    accountingDrivenDevelopment = new Talk(45, 'Accounting-Driven Development', null);
+    woah = new Talk(30, 'Woah', null);
+    sitDownandWrite = new Talk(30, 'Sit Down and Write', null);
+    pairProgrammingvsNoise = new Talk (45, 'Pair Programming vs Noise', null);
+    railsMagic = new Talk (60, 'Rails Magic', null);
+    rubyonRailsWhyWeShouldMoveOn = new Talk (60, 'Ruby on Rails: Why We Should Move On', null);
+    clojureAteScalaonmyproject = new Talk(45, 'Clojure Ate Scala (on my project)', null);
+    programmingintheBoondocksofSeattle = new Talk(30, 'Programming in the Boondocks of Seattle', null);
+    rubyVsClojureforBackEndDevelopment = new Talk(30, 'Ruby vs. Clojure for Back-End Development', null );
+    rubyonRailsLegacyAppMaintenance = new Talk (60,'Ruby on Rails Legacy App Maintenance', null);
+    aWorldWithoutHackerNews = new Talk (30, 'A World Without HackerNews', null);
+    userInterfaceCSSinRailsApps = new Talk(30, 'User Interface CSS in Rails Apps', null);
 });
 
 describe("Conference Component can keep track of it's children Tracks", () => {
@@ -64,5 +110,16 @@ describe("Track Component can keep track of it's children Talks", () => {
     });
 });
 
+// Method buildTrackList() from Conference.ts
+describe("Method buildTrackList() works as Expected", () => {
+    test('buildTrackList() gives out the same output as the one provided in the PDF document', () => {
+        // Add Talks to arrayOfTalks
+        let arrayOfTalks = [writingFastTestsAgainstEnterpriseRails, overdoingitinPython, luafortheMasses, rubyErrorsfromMismatchedGemVersions, 
+            commonRubyErrors, railsforPythonDevelopers, communicatingOverDistance, accountingDrivenDevelopment, woah, sitDownandWrite, 
+            pairProgrammingvsNoise, railsMagic, rubyonRailsWhyWeShouldMoveOn, clojureAteScalaonmyproject, programmingintheBoondocksofSeattle, 
+            rubyVsClojureforBackEndDevelopment, rubyonRailsLegacyAppMaintenance, aWorldWithoutHackerNews, userInterfaceCSSinRailsApps];
 
-
+        onlyOneConference.buildTrackList(arrayOfTalks);
+        expect(onlyOneConference.tracks).toHaveLength(2);
+    });
+});
