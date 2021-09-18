57219c468db7d1730bf62ac3158da3c54707bd67
Xavier Marques <xavierpmarques@gmail.com>
2021-09-18 10:38:09 +0100

    Add ListOfTalks.tsx Component
    * Renders an array of talks to the DOM

---
+++ src/components/listGenerators/ListOfTalks.tsx
@@ -0,0 +1,32 @@
+import Talk from '../Talk';
+
+interface listOfTalksProps {
+    arrayOfTalks: Talk[];
+    session: string;
+}
+
+const ListOfTalks = (props: listOfTalksProps) => {
+    const arrayOfTalks: Talk[] = props.arrayOfTalks;
+    const session: string = props.session
+    let className: string;
+
+    if(session === 'morning') {
+        className = 'morning-talk-container';
+    }
+    else {
+        className = 'afternoon-talk-container';
+    }
+
+    // Map arrayOfTracks to return an new array of divs that contain Talk Components
+    const talksList = arrayOfTalks.map((talk) => (
+        <div key={talk.title} className={className}>
+            <Talk talk={talk}/>
+        </div>
+    ));
+    
+    return <>{
+        [talksList]
+    }</>
+}
+
+export default ListOfTalks
