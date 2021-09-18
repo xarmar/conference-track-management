f744c1e701cb005e3ba6f3b967a72c4e7cd827fa
Xavier Marques <xavierpmarques@gmail.com>
2021-09-18 10:35:50 +0100

    Add basic logic to render() a Track Component

--- src/components/Track.tsx
+++ src/components/Track.tsx
@@ -1,5 +1,6 @@
 import {Component} from 'react'
 import { addMinutesToDate, convertToAmPm, createDate } from '../dateManipulation/timeOperations';
+import ListOfTalks from './listGenerators/ListOfTalks';
 import Talk from './Talk';
 
 class Track extends Component <any, any> {
@@ -77,7 +78,49 @@ class Track extends Component <any, any> {
     }
 
     render() {
-        return null
+
+    const track: Track = this.props.track
+
+    // Get Track's Number
+    const trackNumber: number = track.trackNumber;
+    
+    // Get Track's Lunch and Networking event start times
+    const lunchStartTime = track.lunchHourStartTime;
+    const networkingStartTime = track.networkingEventStartTime
+
+    // Get Track's Morning and Afternoon Talks
+    const morningTalks: Talk[] = track.sessions.morning.talks;
+    const afternoonTalks: Talk[] = track.sessions.afternoon.talks;
+
+        return (
+            <div>
+                <div className="track-title-container">
+                    <h1> Track #${trackNumber}</h1>
+                </div>
+                <div className="track-morning-container">
+                    <div className="track-morning-icon-container">
+                        <p> Icon For Morning Goes Here </p>
+                    </div>
+                    <div className="track-morning-talks-container">
+                        <ListOfTalks session="morning" arrayOfTalks={morningTalks}/>
+                    </div>
+                </div>
+                <div className="track-lunch-time-container">
+                    <p>{convertToAmPm(lunchStartTime)} - Lunch</p>
+                </div>
+                <div className="track-afternoon-container">
+                    <div className="track-afternoon-icon-container">
+                        <p> Icon For Morning Goes Here </p>
+                    </div>
+                    <div className="track-afternoon-talks-container">
+                        <ListOfTalks session="afternoon" arrayOfTalks={afternoonTalks}/>
+                    </div>
+                </div>
+                <div className="track-networking-event-time-container">
+                    <p>{networkingStartTime} - Networking Event</p>
+                </div>
+            </div>
+        )
     }
 }
 
