5fd9427418d7ba5a991436ea895db8baa17daa1f
Xavier Marques <xavierpmarques@gmail.com>
2021-09-20 23:25:02 +0100

    Set Methods for mounting and unmounting Components

--- src/components/ControllerApi.js
+++ src/components/ControllerApi.js
@@ -1,10 +1,10 @@
 import React, { Component, useState } from 'react';
-import Grid from '@mui/material/Grid';
-import AppHeader from './AppHeader';
 import { Container } from '@mui/material';
-import Form from './Form';
+import Form from './SliderBuilder';
 import Conference from './Conference';
-import TextFieldGenerator from './TextFieldGenerator';
+import TextFieldGenerator from './TextFieldBuilder';
+import MainMenu from './MainMenu';
+import { Box } from '@mui/system';
 
 class ControllerApi extends Component {
 
@@ -13,34 +13,71 @@ class ControllerApi extends Component {
 
         // Keeps state of what should be mounted unmounted
         this.state = {
-            showForm: true,
+            conference: "",
             showConference: false,
-            showTextFieldOption: true,
-            conference: ""
+            showMainMenu: true,
+            showSliderBuilder: false,
+            showTextFieldBuilder: false
         }
 
+    this.renderMainMenuComponent = this.renderMainMenuComponent.bind(this);
+    this.unmontMainMenuComponent = this.unmontMainMenuComponent.bind(this);
 
-    this.unmountFormComponent = this.unmountFormComponent.bind(this);
+    this.renderSliderBuilderComponent = this.renderSliderBuilderComponent.bind(this);
+    this.unmountSliderBuilderComponent = this.unmountSliderBuilderComponent.bind(this);
+
+    this.renderTextFieldBuilderComponent = this.renderTextFieldBuilderComponent.bind(this);
+    this.unmountTextFieldBuilderComponent = this.unmountTextFieldBuilderComponent.bind(this);
+
     this.renderConferenceComponent = this.renderConferenceComponent.bind(this);
-    this.unMountConferenceMountForm = this.unMountConferenceMountForm.bind(this);
-    this.unmountTextFieldOptionComponent = this.unmountTextFieldOptionComponent(this);
+    this.unMountConferenceComponent = this.unMountConferenceComponent.bind(this);
     }
 
-    // Unmounts the Form Component
-    unmountFormComponent() {
+    // MAIN MENU COMPONENT ---------------------------------
+    // Renders Main Menu the DOM
+    renderMainMenuComponent() {
         this.setState({
-            showForm: false
+            showMainMenu: true
+        });
+    }
+    // Unmount Main Menu Component
+    unmontMainMenuComponent() {
+        this.setState({
+            showMainMenu: false
         });
     }
 
-    // Unmounts the TextFieldOption Component
-    unmountTextFieldOptionComponent() {
+
+    // SLIDER COMPONENT ---------------------------------
+    // Renders Slider Builder to the DOM
+    renderSliderBuilderComponent() {
         this.setState({
-            showTextFieldOption: false
+            showSliderBuilder: true
+        });
+    }
+    // Unmounts the Slider Component
+    unmountSliderBuilderComponent() {
+        this.setState({
+            showSliderBuilder: false
         });
     }
 
-    // Renders the Conference's Tracks to the UI
+    // TEXTFIELD COMPONENT ---------------------------------
+    // Renders Text Field Builder to the DOM
+    renderTextFieldBuilderComponent() {
+        this.setState({
+            showTextFieldBuilder: true
+        });
+    }
+    // Unmounts TextFieldBuilder Component
+    unmountTextFieldBuilderComponent() {
+        this.setState({
+            showTextFieldBuilder: false
+        });
+    }
+
+    // CONFERENCE COMPONENT ---------------------------------
+    // Renders Conference Component to the DOM
     renderConferenceComponent(arrayOfTalks) {
         const newConference = new Conference([], null);
         newConference.buildTrackList(arrayOfTalks);
@@ -49,12 +86,10 @@ class ControllerApi extends Component {
             showConference: true,
         })
     }
-
     // Takes the user back to the initial page
-    unMountConferenceMountForm() {
+    unMountConferenceComponent() {
         this.setState({
             showConference: false,
-            showForm: true,
             conference: ""
         })
     }
@@ -62,14 +97,24 @@ class ControllerApi extends Component {
     render() {
         return (
             <Container>
-                <Grid container direction="row">
-                    <AppHeader /> 
-                </Grid>
-                <Grid id="main-content-container-grid" container direction="row" >
-                    {this.state.showForm && <Form unmountFormComponent={this.unmountFormComponent} renderConferenceComponent={this.renderConferenceComponent} />}
-                    {this.state.showConference && <Conference conference={this.state.conference} unMountConferenceMountForm={this.unMountConferenceMountForm} /> }
-                    {this.state.showTextFieldOption && <TextFieldGenerator unmountTextFieldOptionComponent={this.unmountTextFieldOptionComponent} />}
-                </Grid>
+                <Box id="main-content-container-grid" container direction="row" className="margin-auto" >
+                    {this.state.showMainMenu && <MainMenu 
+                    renderTextFieldBuilderComponent={this.renderTextFieldBuilderComponent}
+                    renderSliderBuilderComponent={this.renderSliderBuilderComponent} />}
+
+                    {this.state.showSliderBuilder && <Form 
+                    unmountSliderBuilderComponent={this.unmountSliderBuilderComponent} 
+                    renderConferenceComponent={this.renderConferenceComponent} />}
+
+                    {this.state.showConference && <Conference 
+                    conference={this.state.conference} 
+                    unMountConferenceMountForm={this.unMountConferenceMountForm} 
+                    renderMainMenuComponent={this.renderMainMenuComponent} />}
+
+                    {this.state.showTextFieldOption && <TextFieldGenerator 
+                    unmountTextFieldOptionComponent={this.unmountTextFieldOptionComponent} 
+                    renderConferenceComponent={this.renderConferenceComponent} />}
+                </Box>
             </Container>
         )
     }
